837afefab94952ca40f2cfb566e7d6d92b00878f
angelabreyeressiam <angela_breyer-essiam28@milton.edu>
2026-05-19 09:06:48 -0400

    In class 5/19

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,17 @@ public class Main {
         Main blackjack = new Main();
     }
 
-    public Main(){
+    public Main() {
         deck = new Card[52];
-        for(int i =0; i< deck.length; i ++){
+        for (int i = 0; i < deck.length; i++) {
             if (i < 13) {
-                deck[i] = new Card(i%13, "Spades", false);
-            } else if (i < 26){
-                deck[i] = new Card(i%13, "Diamonds", false);
-            } else if (i<39){
-                deck[i] = new Card(i%13, "Hearts", false);
-            } else if (i< 52){
-                deck[i] = new Card(i%13, "Clubs", false);
+                deck[i] = new Card(i % 13, "Spades", false);
+            } else if (i < 26) {
+                deck[i] = new Card(i % 13, "Diamonds", false);
+            } else if (i < 39) {
+                deck[i] = new Card(i % 13, "Hearts", false);
+            } else if (i < 52) {
+                deck[i] = new Card(i % 13, "Clubs", false);
             }
 
         }
@@ -30,11 +30,11 @@ public class Main {
         printDeck();
 
         playerOne.addCard(deck[numCards]);
-        numCards ++;
+        numCards++;
         playerOne.addCard(deck[numCards]);
-        numCards ++;
+        numCards++;
         dealer.addCard(deck[numCards]);
-        numCards ++;
+        numCards++;
         playerOne.handTotal();
         dealer.handTotal();
         playerOne.printPlayer();
@@ -44,24 +44,55 @@ public class Main {
 
         while (true) {
             String input = sc.nextLine();
-            if (input.equals("h")){
+            if (input.equals("h")) {
                 playerOne.addCard(deck[numCards]);
-                numCards ++;
+                numCards++;
                 playerOne.handTotal();
                 playerOne.printPlayer();
-                if(playerOne.handTotal > 21){
+                if (playerOne.handTotal > 21) {
+                    playerOne.printPlayer();
+                    dealer.printDealer();
                     System.out.println("Player One loses!");
                 }
-             else if(input.equals("s")){
-                    System.out.println("Player One choses to stand!");
+                else if(playerOne.handTotal == 21){
+                    playerOne.printPlayer();
+                    dealer.printDealer();
+                    System.out.println("Player One wins!");
                 }
 
             }
+            if (input.equals("s")) {
+                    System.out.println("Player One chooses to stand!");
+                    playerOne.handTotal();
+                    playerOne.printPlayer();
+                    if (playerOne.handTotal > 21) {
+                        System.out.println("Player One loses!");
+                    }
+                    if (dealer.handTotal < 16){
+                        dealer.addCard(deck[numCards]);
+                        numCards++;
+                        dealer.handTotal();
+                        dealer.printDealer();
+                     if (dealer.handTotal > 21) {
+                         System.out.println("Dealer loses! Player One wins!");
+                     }
+                     else if (dealer.handTotal > playerOne.handTotal){
+                         System.out.println("Dealer Wins! Boohoo!");
+                     }
+                     else if (dealer.handTotal == playerOne.handTotal){
+                         System.out.println("Wow its a tie!");
+                     }
 
+
+                    }
+                    break;
+
+                }
+
+
+            }
 
         }
-
-    }
 
     public void printDeck(){
         for(int i = 0; i < deck.length; i++){
